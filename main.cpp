03944fa5a1c4402a10c6681479e98a8f3ac4dfe6
Slava <madbadrat301@yandex.ru>
2023-12-12 21:09:12 +0700

    1. Переделал в один поток 2. Добавил маски 3. Перенес дескрипторы в цикл

--- main.cpp
+++ main.cpp
@@ -3,16 +3,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/select.h>
-#include <pthread.h>
 
 #define MAX_CLIENTS 1
 
 int server_sock = -1;
 int client_socket = -1;
-pthread_mutex_t mutex;
 volatile sig_atomic_t running = 1;
 
 void handle_signal(int sig) {
@@ -20,29 +19,6 @@ void handle_signal(int sig) {
         printf("Получен сигнал SIGHUP. Завершение\n");
         running = 0;
     }
-}
-
-void* connection_handler(void* socket_desc) {
-    int client_sock = *(int*)socket_desc;
-    char buffer[1024];
-    ssize_t bytes_received;
-
-    while ((bytes_received = recv(client_sock, buffer, sizeof(buffer), 0)) > 0) {
-        pthread_mutex_lock(&mutex);
-        printf("Получены данные от клиента: %ld байт\n", bytes_received);
-        pthread_mutex_unlock(&mutex);
-    }
-
-    if (bytes_received == 0) {
-        pthread_mutex_lock(&mutex);
-        printf("Клиент отключен\n");
-        pthread_mutex_unlock(&mutex);
-    } else {
-        perror("recv");
-    }
-
-    close(client_sock);
-    return NULL;
 }
 
 int main() {
@@ -54,9 +30,6 @@ int main() {
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = handle_signal;
     sigaction(SIGHUP, &sa, NULL);
-
-    // Инициализация мьютекса
-    pthread_mutex_init(&mutex, NULL);
 
     // Создание серверного сокета
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -82,57 +55,76 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    fd_set read_fds;
-    FD_ZERO(&read_fds);
-    FD_SET(server_sock, &read_fds);
+    sigset_t blockedMask;
+    sigset_t origMask;
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+    sigprocmask(SIG_BLOCK, &blockedMask, &origMask);
 
     while (running) {
+        fd_set read_fds;
+        FD_ZERO(&read_fds);
+        FD_SET(server_sock, &read_fds);
+
         int max_fd = server_sock;
 
-        // Добавление клиентского сокета в набор, если подключен
         if (client_socket != -1) {
             FD_SET(client_socket, &read_fds);
             max_fd = (client_socket > max_fd) ? client_socket : max_fd;
         }
 
-        // Ожидание активности на одном из сокетов или сигналах
-        pselect(max_fd + 1, &read_fds, NULL, NULL, NULL, NULL);
+        struct timespec timeout = { 1, 0 };  // Ожидание событий ввода-вывода 1 секунда
 
-        // Проверка входящего соединения
+        int activity = pselect(max_fd + 1, &read_fds, NULL, NULL, NULL, &origMask);
+
+        if (activity < 0) {
+            if (errno == EINTR) {
+                continue;  // Продолжаем, если сигнал был обработан
+            } else {
+                perror("pselect");
+                break;
+            }
+        }
+
+        if (activity == 0) {
+            // Таймаут, ничего не произошло
+            continue;
+        }
+
         if (FD_ISSET(server_sock, &read_fds)) {
             if ((new_socket = accept(server_sock, NULL, NULL)) < 0) {
                 perror("accept");
                 exit(EXIT_FAILURE);
             }
 
-            // Закрытие дополнительных соединений
             if (client_socket != -1) {
                 close(new_socket);
             } else {
-                pthread_mutex_lock(&mutex);
                 printf("Принято новое соединение\n");
-                pthread_mutex_unlock(&mutex);
-
                 client_socket = new_socket;
-
-                // Создание потока для обработки клиентского соединения
-                pthread_t thread;
-                if (pthread_create(&thread, NULL, connection_handler, (void*)&client_socket) < 0) {
-                    perror("pthread_create");
-                    exit(EXIT_FAILURE);
-                }
-                pthread_detach(thread);
             }
         }
 
-        // Очистка набора для следующей итерации
-        FD_ZERO(&read_fds);
+        if (client_socket != -1 && FD_ISSET(client_socket, &read_fds)) {
+            char buffer[1024];
+            ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+
+            if (bytes_received > 0) {
+                printf("Получены данные от клиента: %ld байт\n", bytes_received);
+            } else if (bytes_received == 0) {
+                printf("Клиент отключен\n");
+                close(client_socket);
+                client_socket = -1;
+            } else {
+                perror("recv");
+            }
+        }
     }
 
-    // Закрытие сокетов и уничтожение мьютекса перед выходом
+    // Закрытие сокетов перед выходом
     close(server_sock);
     close(client_socket);
-    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
+
